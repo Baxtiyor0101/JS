467da7887a206329cd3409e9074668a649b26d6f
Baxtiyor0101 <oqonboyevb@gmail.com>
2023-10-24 11:15:11 +0500

    birinchidars

---
+++ birinchi dars/index.js
@@ -0,0 +1,36 @@
+// js
+// data type
+// 1 primitive
+// 2 noneprimitive
+// Symbol
+// BigIng
+// let ism;
+// console.log(ism);
+// let student = {
+//     ism:"Nodir",
+//     familya:"Sobirov",
+//     merried:null,
+//     address:"tashkent"
+// }
+// console.log(student);
+
+// let age=null;
+// console.log(typeof( age)   );
+// console.log(9/"jhjkk");
+
+// alert("this is just test");
+
+// 1 misol
+// birinchi browserda ishlash mumkunmi degan sorov
+//  keyin mening ismim "    " , men "   "da oqiyman , yoshim "   " da, men "   " bolmoqchiman
+// isming nima?
+// qatta oqiysan?
+// yoshing nechada? 
+// kim  bolmoqchisan?
+
+// confirm("kdjkdfjbvk")  
+// let ism = prompt("isming nima ?")
+
+// 2 misol
+//  - + / *
+// kvadratning tomini a berilgan uning perematri Pni va yuzasi Sni toping
