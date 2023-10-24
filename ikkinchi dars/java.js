833e42b7f3673c8441dc750496508c885b6a9b00
Baxtiyor0101 <oqonboyevb@gmail.com>
2023-10-24 12:16:25 +0500

    test

---
+++ ikkinchi dars/java.js
@@ -0,0 +1,13 @@
+// operator 
+// arifmetik + -  * / % **
+// taqqoslash > < <= >= != !== == ===
+// logical && || ! 
+// console.log(4**1/2);
+// console.log(12!=12);
+// let num;
+// let b = "num"&&12;
+// let a = 0||false||0||null||" "||undefined;
+// console.log(b);
+
+let a = 2 && 0 && 3 || 0 || false && 67 || 12
+console.log(a);
